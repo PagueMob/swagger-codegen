d262045c802ad13d538a1647b8fa81acf8e71f07
Ricardo Franco <rmf.franco@gmail.com>
2012-11-14 10:30:30 -0200

    added SwaggerType Enumeration

---
+++ src/main/scala/com/wordnik/swagger/codegen/util/SwaggerType.scala
@@ -0,0 +1,18 @@
+package com.wordnik.swagger.codegen.util
+
+object SwaggerType {
+  val Byte = "byte"
+  val Boolean = "boolean"
+  val Int = "int"
+  val Long = "long"
+  val Float = "float"
+  val Double = "double"
+  val String = "string"
+  val Date = "Date"
+  val List = "List"
+  val Set = "Map"
+  val Array = "Array"
+  val Map = "Map"
+  val Object = "Object"
+  val Void = "void"
+}
